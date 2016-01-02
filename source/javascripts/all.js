9697b877c169d45b02d75015bd2e3760f3700bda
Steven Bailey <stevenbailey@D-10-150-14-117.dhcp4.washington.edu>
2016-01-02 13:08:43 -0800

    isotope

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,29 @@
-//= require_tree .
+//= require "isotope.min"
+//= require "imagesloaded.pkgd.min"
+
+
+    $(document).ready( function() {
+      // init Isotope
+      var $grid = $('.review-comments').isotope({
+        itemSelector: '.review-comment',
+        percentPosition: true,
+        masonry: {
+          columnWidth: '.review-comment-sizer'
+        }
+      });
+      // layout Isotope after each image loads
+      $grid.imagesLoaded().progress( function() {
+        $grid.isotope('layout');
+      });  
+
+
+      $('.review-comment-nav').on( 'click', '.review-comment-filter', function() {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+
+        $('.review-comment-filter').removeClass('current');
+        $(this).addClass('current');
+      });
+
+
+    });
